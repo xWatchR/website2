07cb09ecba878b38dacb18646edf3bccb9e963e9
xWatchR <sebastianmissler@web.de>
2023-03-09 22:50:05 +0100

    menu button and bg responsive

---
+++ src/scripts/navAndMenuText.js
@@ -0,0 +1,19 @@
+const text = document.querySelector('.text')
+const button = document.getElementById('nav-toggle')
+
+const toggleNav = () => {
+  document.body.dataset.nav =
+    document.body.dataset.nav === 'true' ? 'false' : 'true'
+}
+
+function playAnimationEvery10Seconds() {
+  setInterval(() => {
+    text.classList.add('animate')
+  }, 4000)
+
+  text.addEventListener('animationend', () => {
+    text.classList.remove('animate')
+  })
+}
+
+playAnimationEvery10Seconds()
--- src/scripts/trailer.js
+++ src/scripts/trailer.js
@@ -1,62 +1,62 @@
-const trailer = document.getElementById("trailer");
+const trailer = document.getElementById('trailer')
 
 const animateTrailer = (e, interacting) => {
   const x = e.clientX - trailer.offsetWidth / 2,
-    y = e.clientY - trailer.offsetHeight / 2;
+    y = e.clientY - trailer.offsetHeight / 2
 
   const keyframes = {
-    transform: `translate(${x}px, ${y}px) scale(${interacting ? 5 : 1})`,
-  };
+    transform: `translate(${x}px, ${y}px) scale(${interacting ? 5 : 1})`
+  }
 
-  trailer.animate(keyframes, { duration: 800, fill: "forwards" });
-};
+  trailer.animate(keyframes, { duration: 800, fill: 'forwards' })
+}
 
 const getTrailerClass = (type) => {
   switch (type) {
-    case "twitch":
-      return "fa-brands fa-twitch";
-    case "twitter":
-      return "fa-brands fa-twitter";
-    case "insta":
-      return "fa-brands fa-instagram";
-    case "video":
-      return "fa-solid fa-play";
+    case 'twitch':
+      return 'fa-brands fa-twitch'
+    case 'twitter':
+      return 'fa-brands fa-twitter'
+    case 'insta':
+      return 'fa-brands fa-instagram'
+    case 'video':
+      return 'fa-solid fa-play'
     default:
-      return "fa-solid fa-link";
+      return 'fa-solid fa-link'
   }
-};
+}
 
 const getTrailerColor = (type) => {
   switch (type) {
-    case "twitch":
-      return "rgba(100, 65, 164, 0.7)";
-    case "twitter":
-      return "rgba(29, 160, 242, 0.7)";
-    case "insta":
-      return "radial-gradient(circle at 30% 107%, rgba(253, 244, 151, 0.7) 0%, rgba(253, 244, 151, 0.7) 5%, rgba(253, 88, 73, 0.7) 45%, rgba(214, 36, 158, 0.7) 60%, rgba(40, 89, 235, 0.7) 90%)";
-    case "link":
-      return "rgba(83, 130, 232, 0.7)";
-    case "video":
-      return "red";
+    case 'twitch':
+      return 'rgba(100, 65, 164, 0.7)'
+    case 'twitter':
+      return 'rgba(29, 160, 242, 0.7)'
+    case 'insta':
+      return 'radial-gradient(circle at 30% 107%, rgba(253, 244, 151, 0.7) 0%, rgba(253, 244, 151, 0.7) 5%, rgba(253, 88, 73, 0.7) 45%, rgba(214, 36, 158, 0.7) 60%, rgba(40, 89, 235, 0.7) 90%)'
+    case 'link':
+      return 'rgba(83, 130, 232, 0.7)'
+    case 'video':
+      return 'red'
     default:
-      return "black";
+      return 'black'
   }
-};
+}
 
 window.onmousemove = (e) => {
-  const interactable = e.target.closest(".interactable");
-  interacting = interactable !== null;
+  const interactable = e.target.closest('.interactable')
+  interacting = interactable !== null
 
-  const icon = document.getElementById("trailer-icon");
-  const trailer = document.getElementById("trailer");
+  const icon = document.getElementById('trailer-icon')
+  const trailer = document.getElementById('trailer')
 
-  animateTrailer(e, interacting);
+  animateTrailer(e, interacting)
 
-  trailer.dataset.type = interacting ? interactable.dataset.type : "";
-  trailer.style.background = interacting ? interactable.dataset.type : "";
+  trailer.dataset.type = interacting ? interactable.dataset.type : ''
+  trailer.style.background = interacting ? interactable.dataset.type : ''
 
   if (interacting) {
-    icon.className = getTrailerClass(interactable.dataset.type);
-    trailer.style.background = getTrailerColor(interactable.dataset.type);
+    icon.className = getTrailerClass(interactable.dataset.type)
+    trailer.style.background = getTrailerColor(interactable.dataset.type)
   }
-};
+}
